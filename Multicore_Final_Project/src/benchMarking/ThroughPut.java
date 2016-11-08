b397af5a72fc22f025c229c9d1191155fc4b8e57
JiaolongYu <joceyu@utexas.edu>
2016-11-07 23:25:11 -0600

    update benchmartk to more runnable

--- Multicore_Final_Project/src/benchMarking/ThroughPut.java
+++ Multicore_Final_Project/src/benchMarking/ThroughPut.java
@@ -1,5 +1,6 @@
-package benchMarking;
+package finalProj.benchMarking;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -8,35 +9,42 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import Interface.ParallelPriorityQueue;
-import multiqueue.MultiQueue;
+import finalProj.ParallelPriorityQueue;
+import finalProj.multiqueue.MultiQueue;
 
 public class ThroughPut {
-	public int providerConsumer(int numOfProviders, int numOfConsumers, ParallelPriorityQueue<Integer> queue){
-		int[] ProviderCounts = new int[numOfProviders];
-		int[] ConsumerCounts = new int[numOfConsumers];
-		ExecutorService threadPool = Executors.newFixedThreadPool(numOfProviders + numOfConsumers);
-		ArrayList<Callable<Integer>> tasks = new ArrayList<>();
-		for( int i = 0; i < numOfProviders; i++) tasks.add(new Provider<Integer>(i, queue, ProviderCounts));
-		for( int i = 0; i < numOfConsumers; i++) tasks.add(new Consumer<Integer>(i, queue, ConsumerCounts));
-		try {
-			threadPool.invokeAll(tasks, 1, TimeUnit.SECONDS);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			threadPool.shutdown();
-		}
-		int sum = 0;
-		for (int i = 0; i < numOfProviders; i++) sum += ProviderCounts[i];
-		for (int i = 0; i < numOfConsumers; i++) sum += ConsumerCounts[i];
-		return sum;
-	}
-	
-	
-	public static void main(String args[]) {
-		ThroughPut tp = new ThroughPut();
-		MultiQueue<Integer> queue = new MultiQueue<>(2, 20);
-		System.out.println(tp.providerConsumer(10,10,queue));
-	}
+    public int providerConsumer(int numOfProviders, int numOfConsumers, ParallelPriorityQueue<Integer> queue){
+        int[] ProviderCounts = new int[numOfProviders];
+        int[] ConsumerCounts = new int[numOfConsumers];
+        ExecutorService threadPool = Executors.newFixedThreadPool(numOfProviders + numOfConsumers);
+        ArrayList<Thread> tasks = new ArrayList<>();
+        for( int i = 0; i < numOfProviders; i++) tasks.add(new Provider(i, queue, ProviderCounts));
+        for( int i = 0; i < numOfConsumers; i++) tasks.add(new Consumer(i, queue, ConsumerCounts));
+        int sum = 0;
+        try {
+            long before = Instant.now().toEpochMilli();
+            threadPool.invokeAll(tasks, 1, TimeUnit.SECONDS);
+            System.out.println(Instant.now().toEpochMilli() - before);
+            for (int i = 0; i < numOfProviders; i++) sum += ProviderCounts[i];
+            for (int i = 0; i < numOfConsumers; i++) sum += ConsumerCounts[i];
+            System.out.println(sum);
+            sum = 0;
+            for (int i = 0; i < numOfProviders; i++) sum += ProviderCounts[i];
+            for (int i = 0; i < numOfConsumers; i++) sum += ConsumerCounts[i];
+            tasks.stream().forEach(e -> e.stop());
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+        }
+        return sum;
+    }
+    
+    
+    public static void main(String args[]) {
+        ThroughPut tp = new ThroughPut();
+        MultiQueue<Integer> queue = new MultiQueue<>(2, 20);
+        System.out.println(tp.providerConsumer(10,10,queue));
+    }
 }
